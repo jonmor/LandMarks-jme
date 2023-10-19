75057a0f1f8ca9e6ec23cd92d55537828c727e83
jonme <jntn.me@gmail.com>
2023-10-18 19:25:50 -0600

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Jonatan Mora Escareño on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
